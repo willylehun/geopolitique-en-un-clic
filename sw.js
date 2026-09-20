3293d6c5358ecd2b6e9f0b301c45ec77e775a113
willylehun <wbuan15@gmail.com>
2026-09-20 16:36:45 +0200

    Ajoute sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'geo-clic-v3';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./data/news.json','./assets/logo.png','./icons/icon-192.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k))))));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
